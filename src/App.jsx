df4e544175492916f1f42487a63cf1787564c30b
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-07 17:08:08 +0545

    app file added for tailwind

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MainLayout } from './layouts';
+import { HomePage, PredictPage, ModelStatusPage, AboutPage } from './pages';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/predict" element={<PredictPage />} />
+          <Route path="/model-status" element={<ModelStatusPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </MainLayout>
+    </Router>
+  );
+}
+
+export default App;
