d7e631b85a1b79d55201001c6c8d7ff1cdbc46d8
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-15 11:12:18 +0545

    Export insights components

---
+++ src/components/insights/index.js
@@ -0,0 +1,8 @@
+export { default as GPAGauge } from './GPAGauge';
+export { default as BehavioralRadar } from './BehavioralRadar';
+export { default as GPADistributionChart } from './GPADistributionChart';
+export { default as RiskDistributionChart } from './RiskDistributionChart';
+export { default as FeatureImportanceChart } from './FeatureImportanceChart';
+export { default as WhatIfScenarios } from './WhatIfScenarios';
+export { default as RiskFactorsList } from './RiskFactorsList';
+export { default as ScholarshipFunnel } from './ScholarshipFunnel';
