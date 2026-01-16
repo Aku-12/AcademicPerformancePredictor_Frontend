317b1d3ffe8d16319c0e28c6ace549d9f9202e41
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-16 22:18:27 +0545

    Update insights component exports

--- src/components/insights/index.js
+++ src/components/insights/index.js
@@ -3,6 +3,5 @@ export { default as BehavioralRadar } from './BehavioralRadar';
 export { default as GPADistributionChart } from './GPADistributionChart';
 export { default as RiskDistributionChart } from './RiskDistributionChart';
 export { default as FeatureImportanceChart } from './FeatureImportanceChart';
-export { default as WhatIfScenarios } from './WhatIfScenarios';
 export { default as RiskFactorsList } from './RiskFactorsList';
 export { default as ScholarshipFunnel } from './ScholarshipFunnel';
