c6f90a9ebdc55a92675969c169da914e216bb4f7
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-20 15:46:10 +0545

    Update central page exports

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { HomePage } from './Home';
+export { PredictPage } from './Predict';
+export { ModelStatusPage } from './ModelStatus';
+export { AboutPage } from './About';
+export { default as InsightsPage } from './Insights';
