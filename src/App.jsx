17b39d73661d8afe22e3c86b9c5581d3aacc3dc8
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-20 15:45:31 +0545

    Update application routing and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainLayout } from './layouts';
-import { HomePage, PredictPage, ModelStatusPage, AboutPage } from './pages';
+import { HomePage, PredictPage, ModelStatusPage, AboutPage, InsightsPage } from './pages';
 import './App.css';
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/predict" element={<PredictPage />} />
+          <Route path="/insights" element={<InsightsPage />} />
           <Route path="/model-status" element={<ModelStatusPage />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
