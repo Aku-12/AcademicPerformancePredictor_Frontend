8ce2a1bab8f93151563776d8cb4326c7e52b24b6
Aku-12 <akashchaudhary205698@gmail.com>
2026-02-03 18:22:19 +0545

    Export application sections for structured routing and composition

---
+++ src/sections/index.js
@@ -0,0 +1,5 @@
+export { HomeSection } from './HomeSection';
+export { PredictSection } from './PredictSection';
+export { InsightsSection } from './InsightsSection';
+export { ModelStatusSection } from './ModelStatusSection';
+export { AboutSection } from './AboutSection';
